4141454907095c31bc6aae985deb3ccdcd0bda90
SammyDods <sammysdods@gmail.com>
2021-06-26 17:07:15 -0400

    TTS translated text

--- background.js
+++ background.js
@@ -21,40 +21,42 @@ requirejs(["axios"], function(axios) {
     chrome.contextMenus.onClicked.addListener((info) => {
         var text = info.selectionText;
     
-        var subscriptionKey = "4de7de4bb9b9468bad08b9b8b507ed99";
-        var endpoint = "https://api.cognitive.microsofttranslator.com";
-    
-        // Add your location, also known as region. The default is global.
-        // This is required if using a Cognitive Services resource.
-        var location = "global";
-    
-        axios({
-            baseURL: endpoint,
-            url: '/translate',
-            method: 'post',
-            headers: {
-                'Ocp-Apim-Subscription-Key': subscriptionKey,
-                'Ocp-Apim-Subscription-Region': location,
-                'Content-type': 'application/json',
-                'X-ClientTraceId': "114a8126-34ab-4439-a108-a60cfe39228c"
-            },
-            params: {
-                'api-version': '3.0',
-                'from': 'en',
-                'to': ['de', 'it']
-            },
-            data: [{
-                'text': text
-            }],
-            responseType: 'json'
-        }).then(function(response){
-            console.log(JSON.stringify(response.data, null, 4));
-        })
-    
         if (toggleStatus) {
+
             //translate to english first
+            var subscriptionKey = "4de7de4bb9b9468bad08b9b8b507ed99";
+            var endpoint = "https://api.cognitive.microsofttranslator.com";
+        
+            // Add your location, also known as region. The default is global.
+            // This is required if using a Cognitive Services resource.
+            var location = "global";
+        
+            axios({
+                baseURL: endpoint,
+                url: '/translate',
+                method: 'post',
+                headers: {
+                    'Ocp-Apim-Subscription-Key': subscriptionKey,
+                    'Ocp-Apim-Subscription-Region': location,
+                    'Content-type': 'application/json',
+                    'X-ClientTraceId': "114a8126-34ab-4439-a108-a60cfe39228c"
+                },
+                params: {
+                    'api-version': '3.0',
+                    'from': 'en',
+                    'to': ['de', 'it']
+                },
+                data: [{
+                    'text': text
+                }],
+                responseType: 'json'
+
+            }).then(function(response){
+                //console.log(JSON.stringify(response.data, null, 4));
+                console.log(response.data[0]["translations"][0]["text"]);
+                chrome.tts.speak(response.data[0]["translations"][0]["text"]); //default is english
+            })
             
-            chrome.tts.speak(text); //default is english
         } else {
             //detect current language
     
