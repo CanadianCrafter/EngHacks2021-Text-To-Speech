4dd08c93064d1d0c6484c6d3135a2ad1618306eb
CanadianCrafter <bryan335667879@gmail.com>
2021-07-19 17:20:40 -0400

    Restructed and reformatted code

--- background.js
+++ background.js
@@ -1,27 +1,4 @@
-var translateStatus = false;
-var yueStatus = false;
-var prevText ="";
-var prevPrevText ="";
-var sameTextNumber = 1;
-var speechRate = 0.6;
-var languageSampleSize = 50;
-
-function setTranslateStatus(status) {
-    translateStatus = status;
-}
-
-function getTranslateStatus() {
-    return translateStatus;
-}
-
-function setYueStatus(status) {
-    yueStatus = status;
-}
-
-function getYueStatus() {
-    return yueStatus;
-}
-
+// Add Chrome context menu
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
         "title": 'Speak',
@@ -30,26 +7,89 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
+// toggles
+var translateStatus = false;
+var yueStatus = false;
+
+//System settings
+var sameTextNumber = 1; //number of consequtive times a piece of text is voiced
+var slowSpeechRate = 0.6; //speech rate on even numbered clicks
+var languageSampleSize = 50; //maximum size of text sampled for language detection
+
+//System variables
+var prevText = "";
+var text = "";
+var lang = 'en';
+var sampleText = "";
+var translatedText = "";
+var prefLang = "";
+var repeatText = false;
+
+//API Stuff
+var subscriptionKey = "4de7de4bb9b9468bad08b9b8b507ed99";
+var endpoint = "https://api.cognitive.microsofttranslator.com";
+var clientTraceId = "114a8126-34ab-4439-a108-a60cfe39228c"
+var location = "global";
+
+
 requirejs(["axios"], function(axios) {
     chrome.contextMenus.onClicked.addListener((info) => {
-        var text = info.selectionText;
-        prevPrevText = prevText;
-        prevText = text;
-        if(prevText==prevPrevText){
+        text = info.selectionText;
+
+        //save text
+        if(text==prevText){
             sameTextNumber++;
         }
         else{
             sameTextNumber=1;
         }
-    
+        repeatText = text==prevText;
+        prevText = text;
+
         var subscriptionKey = "4de7de4bb9b9468bad08b9b8b507ed99";
         var endpoint = "https://api.cognitive.microsofttranslator.com";
         var clientTraceId = "114a8126-34ab-4439-a108-a60cfe39228c"
         var location = "global";
-        var lang = 'en';
     
-        if (translateStatus) {
-            //translate to english first
+        
+        //Voice text in the language it is in
+        if (!translateStatus) {            
+            //gets sample text for language detection
+            sampleText = text;
+            if(text.length>languageSampleSize){
+                sampleText = text.substring(0, languageSampleSize);
+            }
+
+            //detect current language
+            axios({
+                baseURL: endpoint,
+                url: '/detect',
+                method: 'post',
+                headers: {
+                    'Ocp-Apim-Subscription-Key': subscriptionKey,
+                    'Ocp-Apim-Subscription-Region': location,
+                    'Content-type': 'application/json',
+                    'X-ClientTraceId': clientTraceId
+                },
+                params: {
+                    'api-version': '3.0'
+                },
+                data: [{
+                    'text': sampleText
+                }],
+                responseType: 'json'
+            }).then(function(response){
+                
+                var results = JSON.stringify(response.data, null, 4);
+                var obj = JSON.parse(results);
+                lang = obj[0].language.substring(0,2); //takes the first two characters of the language id                
+
+                speak(text,lang);
+               
+            })
+            
+            
+        } else { //translate before speech
             axios({
                 baseURL: endpoint,
                 url: '/translate',
@@ -70,62 +110,60 @@ requirejs(["axios"], function(axios) {
                 responseType: 'json'
 
             }).then(function(response){
-                //console.log(JSON.stringify(response.data, null, 4));
-                console.log(response.data[0]["translations"][0]["text"]);
-                console.log(navigator.language);
+                            
+                prefLang = navigator.language.substring(0, 2);
                 
-                lang = navigator.language.substring(0, 2)
-                
-                if(lang=="zh"&&yueStatus) lang='zh-hk'; //switches to yue
+                translatedText = response.data[0]["translations"][0]["text"];
 
-                if(sameTextNumber%2!=0){
-                    chrome.tts.speak(response.data[0]["translations"][0]["text"], {'lang': lang}); //default is english
-                }
-                else{
-                    chrome.tts.speak(response.data[0]["translations"][0]["text"], {'lang': lang,'rate':speechRate}); //60% speed
-                }
-            })
-            
-        } else {
-            //detect current language
-            var sampleText = text;
-            if(text.length>languageSampleSize){
-                sampleText = text.substring(0, languageSampleSize);
-            }
-            axios({
-                baseURL: endpoint,
-                url: '/detect',
-                method: 'post',
-                headers: {
-                    'Ocp-Apim-Subscription-Key': subscriptionKey,
-                    'Ocp-Apim-Subscription-Region': location,
-                    'Content-type': 'application/json',
-                    'X-ClientTraceId': clientTraceId
-                },
-                params: {
-                    'api-version': '3.0'
-                },
-                data: [{
-                    'text': sampleText
-                }],
-                responseType: 'json'
-            }).then(function(response){
-                var results = JSON.stringify(response.data, null, 4);
-                console.log(results)
-                var obj = JSON.parse(results);
-                console.log(obj[0].language)
-                lang = obj[0].language.substring(0,2);
-                if(lang=="zh"&&yueStatus) lang='zh-hk'; //switches to yue
- 
-
-                if(sameTextNumber%2!=0){
-                    chrome.tts.speak(text, {'lang': lang}); //specify current language
-
-                }
-                else{
-                    chrome.tts.speak(text, {'lang': lang,'rate':speechRate}); //60% speed
-                }
+                speak(translatedText,prefLang);
             })
         }  
     });
 });
+
+//Modifys language region based on dialect selection
+function dialectModifyer(lang){
+    if(lang=="zh"&&yueStatus) return 'zh-hk'; //switches to yue
+
+    else return lang;
+}
+
+//Speaks the text in the language and speed required.
+function speak(text, lang){
+    lang=dialectModifyer(lang);
+   
+    if(sameTextNumber%2!=0){
+        chrome.tts.speak(text, {'lang': lang}); //specify current language
+    }
+    else{
+        chrome.tts.speak(text, {'lang': lang,'rate':slowSpeechRate}); //60% speed
+    }
+
+    //logs
+    console.log(text);
+    console.log(lang);
+    console.log(sameTextNumber%2!=0?"Normal":"Slow");
+    console.log(translateStatus?"Translate ON":"Translate OFF");
+    console.log(yueStatus?"Yue ON":"Yue OFF");
+    console.log("------------------");
+
+    return;
+}
+
+//Setters and Getters
+
+function setTranslateStatus(status) {
+    translateStatus = status;
+}
+
+function getTranslateStatus() {
+    return translateStatus;
+}
+
+function setYueStatus(status) {
+    yueStatus = status;
+}
+
+function getYueStatus() {
+    return yueStatus;
+}
